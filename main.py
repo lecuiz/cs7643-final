67b9c41d5cfcb3a186f59df2077014ade51dd90a
Alexis <alexis@ElMajestico.local>
2025-12-01 12:57:41 -0800

    adding inference

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from final_project.src.train import train, test
 from final_project.src.utils import Config, ArtemisDataset
+from final_project.src.inference import run_inference
 from torchvision import transforms
-from final_project.src.decoders_w_attention.decoder_rnn import ImageCaptioningModel as rnn_model
-from final_project.src.decoders_w_attention.decoder_lstm import ImageCaptioningModel as lstm_model
-from final_project.src.decoders_w_attention.decoder_transformer import ImageCaptioningModel as transformer_model
-from final_project.src.decoders.decoder_rnn import ImageCaptioningModel as vanilla_rnn_model
+from final_project.src.decoders_w_simple_attention.decoder_rnn import ImageCaptioningModel as rnn_model
+from final_project.src.decoders_w_simple_attention.decoder_lstm import ImageCaptioningModel as lstm_model
+from final_project.src.decoders_w_multihead_attention.decoder_transformer import \
+    ImageCaptioningModel as transformer_model
+from final_project.src.decoders_w_multihead_attention.decoder_rnn import ImageCaptioningModel as attention_rnn_model
+from final_project.src.decoders_w_multihead_attention.decoder_lstm import ImageCaptioningModel as attention_lstm_model
+
+# from final_project.src.decoders.decoder_rnn import ImageCaptioningModel as vanilla_rnn_model
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -31,13 +36,14 @@ dataset = ArtemisDataset(Config.MINI_PICKLE_PATH, transform=transform, split='tr
 #     dropout=Config.DROPOUT
 # )
 
-model = vanilla_rnn_model(
+model = attention_lstm_model(
     vocab_size=dataset.vocab_size,
     embed_dim=Config.EMBED_DIM,
     hidden_dim=Config.HIDDEN_DIM,
+    num_heads=Config.NUM_HEADS,
     dropout=Config.DROPOUT
 )
 
-
 train(model=model, dataset=dataset)
 test(model=model, dataset=dataset)
+run_inference(model=model, dataset=dataset)
